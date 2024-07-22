11a163c83729e5e4065af8237a218c80846e8688
Jay Vogt <jay@yukicreative.com>
2024-07-22 14:49:09 -0500

    Update maximum bed size to 235mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1843,8 +1843,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
